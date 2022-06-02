95288ee1789ad1b7803bb0f7d5aebe0acefb64bb
Safwan Backar <safwanbackar29@gmail.com>
2022-06-02 08:45:19 +0530

    table

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const lapCartIncrement = document.getElementById('lapCartIncrement')
 const itemIncrement = document.querySelectorAll('.itemIncrement')
-console.log(itemIncrement);
+const display1 = document.getElementById('display1')
+const button1Plus = document.getElementById('button1Plus')
+
 
 let value = 0;
 itemIncrement.forEach(element =>{
@@ -9,4 +11,10 @@ itemIncrement.forEach(element =>{
     lapCartIncrement.textContent = value;
     lapCartIncrement.style.fontWeight = 'bold'
          })
+})
+
+let value1 = 1;
+button1Plus.addEventListener('click',()=>{
+    value1++;
+    display1.textContent = value1;
 })
